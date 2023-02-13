b29e875ad18fa188f962f5bf6e874f8bad7371f6
Proxtx <39201896+Proxtx@users.noreply.github.com>
2023-02-13 19:49:34 +0100

    add theming

--- static/lib/guiLoader.js
+++ static/lib/guiLoader.js
@@ -1,4 +1,5 @@
 import { loadPack } from "/modules/uibuilder/main.js";
 await loadPack("/modules/material/components/pack.json", {
   urlPrefix: "/modules/material/",
+  customStyleSheets: ["../../../lib/overwrite.css"],
 });
